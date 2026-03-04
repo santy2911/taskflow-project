31a562c2027f52b6b547489802dc855fbc35b607
Santy <santyvg29@gmail.com>
2026-03-04 09:22:54 +0100

    listener

---
+++ app.js
@@ -0,0 +1,9 @@
+const inputTarea = document.getElementById('inputTarea');
+const selectCategoria = document.getElementById('selectCategoria');
+const selectPrioridad = document.getElementById('selectPrioridad');
+const btnAnadir = document.getElementById('btnAnadir');
+const inputBusqueda = document.getElementById('inputBusqueda');
+
+btnAnadir.addEventListener('click', function() {
+    console.log('botón pulsado');
+});
